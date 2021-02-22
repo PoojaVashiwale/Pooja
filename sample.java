db5a4a22dcb293045f12e00eea5744e536f483b7
PoojaVashiwale <32418541+PoojaVashiwale@users.noreply.github.com>
2021-02-22 20:00:17 +0530

    some changes in sample file

--- sample.java
+++ sample.java
@@ -1,5 +1,9 @@
 class sample{
 public static void main(String args[]){
+String name="pooja";  
+  
 System.out.println("Hello everyone!");
+System.out.println("Hi "+name+", welcome to java programming");  
+  
 }
 }
